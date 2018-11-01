47d25a4f43385def5e407f8f88657a986e2b0957
girardi69 <girardi69@gmail.com>
2018-11-01 18:04:26 +0100

    Solo esercizio FCC
    Cambiato di nuovo una volta

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
-function multiplyAll(arr) {
-  var product = 1;
-  // Only change code below this line
-  for (var i=0; i < arr.length; i++) {
-    for (var j=0; j < arr[i].length; j++) {
-      product = product * arr[i][j] ;
-    }}
-  // Only change code above this line
-  return product;
-}
-  
-// Modify values below to test your code
-console.log(multiplyAll([[1,2],[3,4],[5,6,7]]));
+// Setup
+var myArray = [];
+var i = 10;
 
-var arr = [
-  [1,2], [3,4], [5,6], [7,8]
-];
-for (var i=0; i < arr.length; i++) {
-  for (var j=0; j < arr[i].length; j++) {
-    console.log("Ciclo "+i+" "+j);
-    console.log(arr[i])
-    console.log(arr[j]);
-      console.log(arr[i][j]);
-  }
-}
+// Only change code below this line.
+do
+ {
+  myArray.push(i);
+  i++;
+}while (i < 10);
+console.log(myArray);
