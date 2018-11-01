166cf9add11ce57c36223467e060cd3ad2c28df2
girardi69 <girardi69@gmail.com>
2018-11-01 20:22:42 +0100

    esercizio correlazione
    ancora da finire

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-// Setup
-var myArray = [];
-var i = 10;
+//Setup
+var contacts = [
+  {
+      "firstName": "Akira",
+      "lastName": "Laine",
+      "number": "0543236543",
+      "likes": ["Pizza", "Coding", "Brownie Points"]
+  },
+  {
+      "firstName": "Harry",
+      "lastName": "Potter",
+      "number": "0994372684",
+      "likes": ["Hogwarts", "Magic", "Hagrid"]
+  },
+  {
+      "firstName": "Sherlock",
+      "lastName": "Holmes",
+      "number": "0487345643",
+      "likes": ["Intriguing Cases", "Violin"]
+  },
+  {
+      "firstName": "Kristian",
+      "lastName": "Vos",
+      "number": "unknown",
+      "likes": ["JavaScript", "Gaming", "Foxes"]
+  }
+];
+// Only change code below this line
+function lookUpProfile(name, prop){
+  for(var i=0; i < contacts.length; i++) {  
+   if (name === contacts[i].firstName){
+        return contacts[i][prop];
+    }
+  }  
+}
 
-// Only change code below this line.
-do
- {
-  myArray.push(i);
-  i++;
-}while (i < 10);
-console.log(myArray);
+
+// Change these values to test your function
+console.log(lookUpProfile("Sherlock", "lastName"));
