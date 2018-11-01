a6ff0e2e0ae5179890810fcf759e006e3b70c4fa
girardi69 <girardi69@gmail.com>
2018-11-01 17:52:03 +0100

    nuovo commit
    fatto da Wise

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-// Example
-var ourArr = [ 9, 10, 11, 12];
-var ourTotal = 0;
-
-for (var i = 0; i < ourArr.length; i++) {
-  ourTotal += ourArr[i];
+function multiplyAll(arr) {
+  var product = 1;
+  // Only change code below this line
+  for (var i=0; i < arr.length; i++) {
+    for (var j=0; j < arr[i].length; j++) {
+      product = product * arr[i][j] ;
+    }}
+  // Only change code above this line
+  return product;
 }
+  
+// Modify values below to test your code
+console.log(multiplyAll([[1,2],[3,4],[5,6,7]]));
 
-// Setup
-var myArr = [ 2, 3, 4, 5, 6];
-var total = 0;
-for (var i=0; i < myArr.length; i++) {
-total += myArr[i];
+var arr = [
+  [1,2], [3,4], [5,6], [7,8]
+];
+for (var i=0; i < arr.length; i++) {
+  for (var j=0; j < arr[i].length; j++) {
+    console.log("Ciclo "+i+" "+j);
+    console.log(arr[i])
+    console.log(arr[j]);
+      console.log(arr[i][j]);
+  }
 }
-// Only change code below this line
-
-
-console.log(total);
