1e0614c3294c3c41b328b7e657df1bbaa9cf046a
girardi69 <girardi69@gmail.com>
2018-11-02 07:45:22 +0100

    random number between
    new version more sophisticated

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-function randomFraction() {
+// Example
+function ourRandomRange(ourMin, ourMax) {
 
-  // Only change code below this line.
+  return Math.floor(Math.random() * (ourMax - ourMin + 1)) + ourMin;
+}
 
-  return Math.floor(Math.random() * 10);
+ourRandomRange(1, 9);
 
-  // Only change code above this line.
+// Only change code below this line.
+
+function randomRange(myMin, myMax) {
+
+  return Math.floor(Math.random() * (myMax - myMin +1) + myMin); // Change this line
+
 }
 
 // Change these values to test your function
-console.log(randomFraction());
+var myRandom = randomRange(5, 15);
+console.log(myRandom);
