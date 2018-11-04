0034c71c1e400cc225c5a8627fbdfb5479639362
girardi69 <girardi69@gmail.com>
2018-11-04 07:28:32 +0100

    cycle and const
    bla ba

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-
-function checkScope() {
+function printManyTimes(str) {
   "use strict";
-    let i = "function scope";
-    if (true) {
-      let i = "block scope";
-      console.log("Block scope i is: ", i);
-    }
-    console.log("Function scope i is: ", i);
-    return i;
-  }
-console.log(checkScope());
+
+  // change code below this line
+
+  const SENTENCE = str + " is cool!";
+  for(let i = 0; i < str.length; i+=2) {
+    console.log(SENTENCE);
+    console.log(i)
+;  }
+
+  // change code above this line
+
+}
+printManyTimes("freeCodeCamp");
