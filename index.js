ff0d945c1d7f1dacda4e5674d895dfdfc06a0176
girardi69 <girardi69@gmail.com>
2018-11-04 09:25:00 +0100

    ES6: Use Arrow
    Understand the exercise with code testing

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
-function freezeObj() {
-  "use strict";
-  const MATH_CONSTANTS = {
-    PI: 3.14
-  };
-  // change code below this line
-
-Object.freeze(MATH_CONSTANTS);
-  // change code above this line
-  try {
-    MATH_CONSTANTS.PI = 99;
-  } catch( ex ) {
-    console.log(ex);
-  }
-  return MATH_CONSTANTS.PI;
+const magic = function() {
+  // const myVar = "value";
+  return  Date();
 }
-const PI = freezeObj();
-console.log(PI);
+console.log(magic());
