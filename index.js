1dbd5726cbfbba42df5d0c13830b4d3ea0286647
girardi69 <girardi69@gmail.com>
2018-11-04 09:06:09 +0100

    change components of an array
    still to be understood

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-function printManyTimes(str) {
+const s = [5, 7, 2];
+function editInPlace() {
   "use strict";
-
   // change code below this line
-
-  const SENTENCE = str + " is cool!";
-  for(let i = 0; i < str.length; i+=2) {
-    console.log(SENTENCE);
-    console.log(i)
-;  }
+  s[0] = 2;
+  s[1] = 5;
+  s[2] = 7;
+   
+  // s = [2, 5, 7]; <- this is invalid
 
   // change code above this line
-
 }
-printManyTimes("freeCodeCamp");
+
+
+console.log(editInPlace());
