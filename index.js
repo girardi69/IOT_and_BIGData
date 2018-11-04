808791e6f15f7e7d6b83f8abf95dff1bec0c21d9
girardi69 <girardi69@gmail.com>
2018-11-04 09:37:38 +0100

    ES6: Arrow
    That works in fcc editor but not in VS Code ?

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const magic = function() {
-  // const myVar = "value";
-  return  Date();
-}
-console.log(magic());
+const then = new Date();
+const now = () => new Date();
+let count = 0;
+
+console.log(`then: ${then.getSeconds()}`);
+console.log(`now: ${now().getSeconds()}`);
+
+
+const timer = setInterval(function() {
+	count++;	
+  console.log('Two seconds later...');
+	console.log(`then: ${then.getSeconds()}`);
+	console.log(`now: ${now().getSeconds()}`);
+  console.log('----------');
+	if (count === 10) clearInterval(timer);
+}, 2000);
