75776a2817ffa6016987b2fb10960838388d9d50
girardi69 <girardi69@gmail.com>
2018-11-04 17:55:35 +0100

    *** MOZILLA DEVELOPER ***
    Some freestyle on
    .filter
    .includes

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
-const squareList = (arr) => {
-  "use strict";
-  // change code below this line
-  
-  const squaredIntegers = arr.filter( (num) => num > 0 &&  num % parseInt(num) === 0).map ( (num) => Math.pow(num, 2));
+//var words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
 
+//const result = words.filter(word => word.length > 8 );
 
+//console.log(result);
+// expected output: Array ["exuberant", "destruction", "present"]
 
-  // change code above this line
-  return squaredIntegers;
-};
-// test your code
-const squaredIntegers = squareList(realNumberArray);
-console.log(squaredIntegers);
+var words = ['limit', 'elite'];
+
+const result = words.includes("limit");
+
+console.log(result);
+// expected output: Array ["limit", "elite", "present"]
