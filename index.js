ce93c871d3c02c2d390602c7f0a8ac9f224390a7
girardi69 <girardi69@gmail.com>
2018-11-10 09:36:28 +0100

    with let and const, but not very clear the outcome expected

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-var numArray = [];
-for (var i = 0; i < 3; i++) {
-  numArray.push(i);
-}
-console.log(numArray);
-// returns [0, 1, 2]
-console.log(i);
-// returns 3
+function printManyTimes(str) {
+    "use strict";
+  
+    // change code below this line
+  
+    const SENTENCE = str + " is cool!";
+    for(let i = 0; i < str.length; i+=2) {
+      console.log(SENTENCE);
+    }
+  
+    // change code above this line
+  
+  }
+  printManyTimes("freeCodeCamp");
