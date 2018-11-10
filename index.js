45e57e97769fb2e6ee8ef9ca02d9a08b914b9646
girardi69 <girardi69@gmail.com>
2018-11-10 07:34:13 +0100

    Start again from ES6

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-// Developer MOZILLA
+var catName;
+var quote;
+function catTalk() {
+  "use strict";
 
-/*  EXAMPLE FOR .filter
-//var words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
-//const result = words.filter(word => word.length > 8 );
-console.log(result); */
-// expected output: Array ["exuberant", "destruction", "present"]
+  catName = "Oliver";
+  quote = catName + " says Meow!";
 
-/*  EXAMPLE FOR .includes
-var words = ['limit', 'elite'];
-const result = words.includes("limit");
-console.log(result); */
-// expected output: true
-
-//  EXAMPLE FOR .map
-var words = ['limit', 'elite'];
-const result = words.push("cow");
-words.push("This is definitely to test last change");
-console.log(words);
-// expected output: elite
+}
+console.log(catTalk();
