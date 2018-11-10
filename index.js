fd6fbf1a504d752d90100e27b7f072727d9e641a
girardi69 <girardi69@gmail.com>
2018-11-10 09:58:15 +0100

    Exercise resolved with external help

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-function printManyTimes(str) {
-    "use strict";
-  
-    // change code below this line
-  
-    const SENTENCE = str + " is cool!";
-    for(let i = 0; i < str.length; i+=2) {
-      console.log(SENTENCE);
-    }
-  
-    // change code above this line
-  
-  }
-  printManyTimes("freeCodeCamp");
+const s = [5, 7, 2];
+function editInPlace() {
+  "use strict";
+  // change code below this line
+
+ s[0]=2;
+ s[2]=7;
+ s[1]=5;
+return s;
+  // change code above this line
+}
+console.log(editInPlace());
