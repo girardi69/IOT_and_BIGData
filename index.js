399e0ed4e0252910db1efb0546fc1ad995252415
girardi69 <girardi69@gmail.com>
2018-11-11 13:59:44 +0100

    fcc ES6 Set Default Param Solved

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-function greeting(name = "Anonymous") {
-    return "Hello " + name;
-  }
-  console.log(greeting("John")); // Hello John
-  console.log(greeting()); // Hello Anonymous
+
+  const increment = (function() {
+    "use strict";
+    return function increment(number, value = 1) {
+      return number + value;
+    };
+  })();
+  console.log(increment(5, 2)); // returns 7
+  console.log(increment(5)); // returns 6
